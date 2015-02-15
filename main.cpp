8fc928166726aa6aa2132bf4e1d8ab0e1b5f8952
Winslow Mohr <wmohr@bob-omb.cs.clemson.edu>
2015-02-14 22:03:31 -0500

    Added Anti-Aliasing

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string.h>
 #include <unistd.h>
+#include <math.h>
 
 #include <GL/glew.h>
 #include <GL/gl.h>
@@ -11,11 +12,26 @@
 
 #define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
+#define VPASSES 10
+#define JITTER 0.01
+
+double genrand()
+{
+	return(((double)(random()+1))/2147483649.);
+}
+
 struct point {
 	float x;
 	float y;
 	float z;
 };
+
+void viewvolume_shape()
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0,1.0,0.1,20.0);
+}
 
 void setup_the_viewvol()
 {
@@ -23,14 +39,7 @@ void setup_the_viewvol()
 	struct point view;
 	struct point up;
 
-	glEnable(GL_DEPTH_TEST);
-
-	/* specify size and shape of view volume */
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(45.0,1.0,0.1,20.0);
-
-	/* specify position for view volume */
+	// specify position for view volume
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
@@ -41,11 +50,50 @@ void setup_the_viewvol()
 	gluLookAt(eye.x,eye.y,eye.z,view.x,view.y,view.z,up.x,up.y,up.z);
 }
 
+struct point cross(struct point u, struct point v)
+{
+	struct point w;
+	w.x = u.y*v.z - u.z*v.y;
+	w.y = -(u.x*v.z - u.z*v.x);
+	w.z = u.x*v.y - u.y*v.x;
+	return(w);
+}
+
+struct point unit_length(struct point u)
+{
+	double length;
+	struct point v;
+	length = sqrt(u.x*u.x+u.y*u.y+u.z*u.z);
+	v.x = u.x/length;
+	v.y = u.y/length;
+	v.z = u.z/length;
+	return(v);
+}
+
+void jitter_view()
+{
+	struct point eye, view, up, vdir, utemp, vtemp;
+
+	eye.x = -3.0; eye.y = 3.0; eye.z = 6.0;
+	view.x = JITTER*genrand() - 0.5; view.y = JITTER*genrand() + 1.0; view.z = JITTER*genrand();
+	up.x = 0.0; up.y = 1.0; up.z = 0.0;
+	vdir.x = view.x - eye.x; vdir.y = view.y - eye.y; vdir.z = view.z - eye.z;
+	// Calculate correct up vector as orthogonal to vdir and in the plane of
+	// vdir and (0,1,0).
+	vtemp = cross(vdir,up);
+	utemp = cross(vtemp,vdir);
+	up = unit_length(utemp);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(eye.x,eye.y,eye.z,view.x,view.y,view.z,up.x,up.y,up.z);
+}
+
 void draw_stuff()
 {
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 	glDrawArrays(GL_TRIANGLES,0,final_vertices.size());
-	glutSwapBuffers();
+	//glutSwapBuffers();
+	glFlush();
 }
 
 void do_lights()
@@ -96,14 +144,11 @@ const GLuint mybuf = 1;
 void initOGL(int argc, char **argv)
 {
 	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_RGBA|GLUT_DEPTH|GLUT_DOUBLE);
+	glutInitDisplayMode(GLUT_RGBA|GLUT_DEPTH|GLUT_ACCUM);
 	glutInitWindowSize(512,512);
 	glutInitWindowPosition(100,50);
 	glutCreateWindow("my_cool_cube");
 	glewInit();
-	setup_the_viewvol();
-	do_lights();
-	do_material();
 	glBindBuffer(GL_ARRAY_BUFFER,mybuf);
 	glBufferData(GL_ARRAY_BUFFER, out_data_size * sizeof(GLfloat), out_data, GL_STATIC_DRAW);
 	// When using VBOs, the final arg is a byte offset in buffer, not the address,
@@ -114,6 +159,20 @@ void initOGL(int argc, char **argv)
 	glEnableClientState(GL_NORMAL_ARRAY);
 	/* gray background */
 	glClearColor(0.35,0.35,0.35,0.0);
+	glClearAccum(0.0,0.0,0.0,0.0);
+}
+
+void go()
+{
+	int view_pass;
+	glClear(GL_ACCUM_BUFFER_BIT);
+	for(view_pass=0;view_pass<VPASSES;view_pass++){
+		jitter_view();
+		draw_stuff();
+		glFlush();
+		glAccum(GL_ACCUM,1.0/(float)(VPASSES));
+	}
+	glAccum(GL_RETURN,1.0);
 }
 
 void getout(unsigned char key, int x, int y)
@@ -131,7 +190,14 @@ int main(int argc, char **argv)
 {
 	read_obj();
 	initOGL(argc,argv);
-	glutDisplayFunc(draw_stuff);
+	glEnable(GL_DEPTH_TEST);
+	viewvolume_shape();
+	//setup_the_viewvol();
+	jitter_view();
+	do_lights();
+	do_material();
+	//glutDisplayFunc(draw_stuff);
+	glutDisplayFunc(go);
 	glutKeyboardFunc(getout);
 	glutMainLoop();
 	return 0;
